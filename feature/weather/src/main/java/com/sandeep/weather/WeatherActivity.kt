ee409bbcda3c9c204afc1c24d0c9990395346a18
sandeep <sandeepgnair@gmail.com>
2024-06-23 23:04:46 +0100

    Move weather images to sub module, add weather UI

--- feature/weather/src/main/java/com/sandeep/weather/WeatherActivity.kt
+++ feature/weather/src/main/java/com/sandeep/weather/WeatherActivity.kt
@@ -3,6 +3,8 @@ package com.sandeep.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,8 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.sandeep.commoncomponents.theme.FontXLarge
+import com.sandeep.commoncomponents.theme.FontXXLarge
 import com.sandeep.commoncomponents.theme.StandardPadding
+import com.sandeep.commoncomponents.theme.TextColorInverted
 import com.sandeep.commoncomponents.theme.WeatherTrackerTheme
 import com.sandeep.commoncomponents.ui.TopToolbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,17 +52,52 @@ fun WeatherScreenContent(
         }
     ) { innerPadding ->
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            Text(
-                text = "Current Weather",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = StandardPadding, end = StandardPadding, top = StandardPadding)
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.sunny),
+                contentDescription = "Currently sunny",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
             )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                Text(
+                    text = "Currently Sunny",
+                    color = TextColorInverted,
+                    fontSize = FontXLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = StandardPadding,
+                            end = StandardPadding,
+                            top = StandardPadding
+                        )
+                )
+                Text(
+                    text = "25°c",
+                    color = TextColorInverted,
+                    fontSize = FontXXLarge,
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(
+                            start = StandardPadding,
+                            end = StandardPadding,
+                            top = StandardPadding
+                        )
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun WeatherScreenContentPreview() {
+    WeatherScreenContent(
+        onBackClicked = { }
+    )
 }
--- shared/common-components/src/main/java/com/sandeep/commoncomponents/theme/Dimens.kt
+++ shared/common-components/src/main/java/com/sandeep/commoncomponents/theme/Dimens.kt
@@ -1,5 +1,9 @@
 package com.sandeep.commoncomponents.theme
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val StandardPadding = 16.dp
+
+val FontXLarge = 40.sp
+val FontXXLarge = 60.sp
